b03930627dbcc2f50e7515196d267f6132eb7b98
ManMadeMachine <Eetu.Suonpaa@hotmail.com>
2018-11-06 03:31:58 +0200

    Added functionality to play/pause the sample. Added a drop-down for selecting samples, which are still hard-coded in the code.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,14 +7,51 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.canvas = React.createRef();
+    this.state = {
+      isPlaying: false,
+      selectedSample: '',
+      availableSamples: []
+    };
 
+    this.canvas = React.createRef();
+    this.togglePlay = this.togglePlay.bind(this);
+    this.selectSample = this.selectSample.bind(this);
   }
   
   componentDidMount(){
     // TODO: Fetch available sample names from the server and show them in a drop-down etc.
     this.canvasController = new CanvasController(this.canvas.current);
-    this.canvasController.start();
+    this.canvasController.load(`virtual-flesh.mp3`);
+  }
+
+  togglePlay(){
+    const {isPlaying } = this.state;
+
+    if(isPlaying){
+      this.canvasController.pause();
+      this.setState({
+        isPlaying: false,
+      })
+    }
+    else{
+      this.canvasController.play();
+      this.setState({
+        isPlaying: true
+      });
+    }
+  }
+
+  selectSample(e){
+    const sampleName = e.target.value;
+
+    console.log("Selected sample: " + sampleName);
+
+    this.setState({
+      isPlaying: false,
+      selectedSample: sampleName	// TODO: Is this even needed..?
+    });
+
+    this.canvasController.load(sampleName);
   }
 
   render() {
@@ -26,7 +63,18 @@ class App extends Component {
           </p>
         </header>
         <content>
-          <canvas ref={this.canvas} id="canvas" width="640" height="480"></canvas>
+          <select className="sample-list" onChange={this.selectSample}>
+          {/* TODO: Maybe the sample names need to be shown like the filenames. In that case the selectedSample is needed in the state. */}
+            <option value="virtual-flesh.mp3">Virtual Flesh</option> 
+            <option value="Humanfobia-Spheres_into_the_abyss.mp3">Humanfobia - Spheres into the abyss</option>
+            <option value="dtmf.mp3">Modem dial-up sample</option>
+          </select>
+          <div className="canvas-container">
+            <canvas ref={this.canvas} id="canvas" width="640" height="480"></canvas>
+          </div>
+          <div className="controls">
+            <button id="play" className="button play-button" onClick={this.togglePlay}><span>{this.state.isPlaying ? 'Pause' : 'Play'}</span></button>
+          </div>
         </content>
       </div>
     );
--- client/src/visualisation/CanvasController.js
+++ client/src/visualisation/CanvasController.js
@@ -14,27 +14,55 @@ class CanvasController {
 
         this.draw = this.draw.bind(this);
 
+        this.isLoaded = false;
+
     }
 
-    start(){
+    load(fileName){
         console.log("Starting canvas controller...");
+        this.fileName = fileName;
 
         // TODO: Load and prepare the file in it's own function. 
-        fetch('/file/dtmf.mp3').then(response => {
+        // TODO: Check if there already was some sample playing, and flush the data buffer. Now the samples are mixed, since
+        // a new buffer source is being created on sample load. 
+        fetch(`/file/${fileName}`).then(response => {
             return response.arrayBuffer();
         }).then(arrayBuffer => {
             return this.audioCtx.decodeAudioData(arrayBuffer);
         }).then(sampleBuffer => {
-            const sampleSource = this.audioCtx.createBufferSource();
-            sampleSource.buffer = sampleBuffer;
-            sampleSource.connect(this.analyser); // TODO: connect this to audioCtx.destination when the play button is done
+            this.sampleSource = this.audioCtx.createBufferSource();
+            this.sampleSource.buffer = sampleBuffer;
+            this.sampleSource.connect(this.analyser).connect(this.audioCtx.destination); // TODO: connect this to audioCtx.destination when the play button is done
             
-            sampleSource.start(); // TODO: Bind this to a play button etc. to avoid annoyng instant playback on page load.
-            
+            //sampleSource.start(); // TODO: Bind this to a play button etc. to avoid annoyng instant playback on page load.
+           
+            console.log("SampleSource state: " + this.sampleSource);
+            this.isLoaded = true;
             this.draw();
 
         });
         
+    }
+
+    play(){
+        // This is the initial state, the sound sample has been created and loaded, and the context state is 'running'. 
+        // In consecutive calls to this method, the state should be suspended.. *SHOULD* be..
+        if (this.isLoaded && this.audioCtx.state === 'running'){
+            this.sampleSource.start();
+        }
+        else if (this.audioCtx.state === 'suspended'){
+            this.audioCtx.resume().then(() => {
+                console.log("Playing sample");
+            });
+        }
+    }
+
+    pause(){
+        if (this.audioCtx.state === 'running'){
+           this.audioCtx.suspend().then(() => {
+                console.log("Paused sample");
+           })         
+        }
     }
 
     draw(){
@@ -42,6 +70,11 @@ class CanvasController {
         this.analyser.getByteTimeDomainData(this.dataArray);
         this.ctx.fillStyle = 'rgb(10, 120, 150)';
         this.ctx.fillRect(0, 0, this.width, this.height);
+
+        // Draw the name of the file onto the canvas
+        this.ctx.font = '20px Arial';
+        this.ctx.fillStyle = 'rgb(30, 30, 30)'
+        this.ctx.fillText(this.fileName, 20, 30);
 
         this.ctx.beginPath();
         
